1d2bca35ab8af23f26c5b16c8374591e0b856d65
Cordier Eugene <eugene126549@gmail.com>
2023-12-06 09:34:36 +0100

    modif test

--- tests/Controller/Contact/IndexCest.php
+++ tests/Controller/Contact/IndexCest.php
@@ -3,7 +3,6 @@
 namespace App\Tests\Controller\Contact;
 
 use App\Factory\ContactFactory;
-use App\Repository\ContactRepository;
 use App\Tests\Support\ControllerTester;
 
 class IndexCest
@@ -17,11 +16,16 @@ class IndexCest
         $I->seeResponseCodeIs(200);
         $I->amOnRoute('app_contact/show', ['id' => 1]);
     }
-    public function search(ControllerTester $I):void
+
+    public function search(ControllerTester $I): void
     {
         $I->amOnPage('contact/');
         ContactFactory::createMany(2);
         ContactFactory::createOne(['firstName' => 'Joe', 'lastName' => 'Adam']);
         ContactFactory::createOne(['firstName' => 'Adam', 'lastName' => 'Aaaaaaaaaaaaaaa']);
+        $I->fillField("//input[@type='search']","Adam");
+        $I->click("submit");
+        $I->see('Joe');
+        $I->see('Aaaaaaaaaaaaaaa');
     }
 }
