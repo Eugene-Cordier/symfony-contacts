dbe57a9f0a624dcca13bfa21af499ae1be360634
Cordier Eugene <eugene126549@gmail.com>
2023-12-14 09:47:36 +0100

    tri par ordre alphabétique des categories

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
     #[Route('/category', name: 'app_category')]
-    public function index(): Response
+    public function index(Request $request, CategoryRepository $categoryRepository): Response
     {
+        $categories= $categoryRepository->findBy([],['name'=> 'ASC']);
         return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+            'categories' => $categories,
         ]);
     }
 }
