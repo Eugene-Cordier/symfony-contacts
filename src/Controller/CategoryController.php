9d267cb52477d0b6ac47ddcfbb5f80fed0f8c8fb
eugene <eugene.cordier@etudiant.univ-reims.fr>
2023-12-30 18:09:09 +0100

    22.5 ajout restriction d'acces

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -8,7 +8,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('IS_AUTHENTICATED_FULLY')]
 class CategoryController extends AbstractController
 {
     #[Route('/category', name: 'app_category')]
