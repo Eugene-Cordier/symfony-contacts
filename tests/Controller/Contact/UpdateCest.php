9f3e9c29188721031f53298fb50f5a46a8986251
eugene <eugene.cordier@etudiant.univ-reims.fr>
2023-12-31 12:41:18 +0100

    22.5 modif test, ajout vérification admin connecté

--- tests/Controller/Contact/UpdateCest.php
+++ tests/Controller/Contact/UpdateCest.php
@@ -3,12 +3,16 @@
 namespace App\Tests\Controller\Contact;
 
 use App\Factory\ContactFactory;
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class UpdateCest
 {
-    public function form(ControllerTester $I): void
+    public function formShowsContactDataBeforeUpdating(ControllerTester $I): void
     {
+        $admin = UserFactory::createOne(['roles' => ['ROLE_ADMIN']]);
+        $adminObj = $admin->object();
+        $I->amLoggedInAs($adminObj);
         ContactFactory::createOne([
             'firstname' => 'Homer',
             'lastname' => 'Simpson',
