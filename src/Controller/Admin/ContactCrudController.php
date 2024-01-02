0c3ea4999f0fd90f8655294f851c0ec2376e0ac7
eugene <eugene.cordier@etudiant.univ-reims.fr>
2024-01-02 23:41:06 +0100

    23.1 modification configureFields()

--- src/Controller/Admin/ContactCrudController.php
+++ src/Controller/Admin/ContactCrudController.php
@@ -3,9 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Contact;
+use App\Repository\CategoryRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TelephoneField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ContactCrudController extends AbstractCrudController
@@ -15,14 +17,23 @@ class ContactCrudController extends AbstractCrudController
         return Contact::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('firstname'),
+            TextField::new('lastname'),
+            TextField::new('email'),
+            TelephoneField::new('phone'),
+            AssociationField::new('category')->setFormTypeOptions([
+                'choice_label' => 'name',
+                'query_builder' => function (CategoryRepository $categoryRepository) {
+                    return $categoryRepository->createQueryBuilder('c')
+                        ->orderBy('c.name', 'ASC');
+                },
+            ])->formatValue(function ($value) {
+                return $value?->getName();
+            }),
         ];
     }
-    */
 }
